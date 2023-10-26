1cbfcfc3873dc325ba2787e9074336b476dfe155
BhathiyaVandebona <malingabhathiya@gmail.com>
2023-10-26 18:56:10 +0530

    removed useless services

--- microservices_dev/authentication_service/index.js
+++ microservices_dev/authentication_service/index.js
@@ -1,8 +1,41 @@
 const express = require('express');
-require('dotenv').config();
-
 const app = express();
+require('dotenv').config();
+const data = require('./schema/data.json');
+
+app.get('/', (req, res) => {
+  console.log('sending all the users');
+  console.log(data);
+  res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+  console.log('sending all the users');
+  console.log(data);
+  res.send('Hello World!');
+});
+
+app.get('/user/:id', (req, res) => {
+  console.log(`Request from: ${req.hostname}`);
+  console.log('sending all the users');
+  res.send('Hello World!');
+});
+
+app.post('/user/:id', (req, res) => {
+  console.log(req.hostname);
+  res.send('Hello World!');
+});
+
+app.put('/user/:id', (req, res) => {
+  console.log(req.hostname);
+  res.send('Hello World!');
+});
+
+app.patch('/user/:id', (req, res) => {
+  console.log(req.hostname);
+  res.send('Hello World!');
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`listening on http://localhost:${process.env.PORT}`);
+  console.log(`Listening on port http://localhost:${process.env.PORT}`);
 });
