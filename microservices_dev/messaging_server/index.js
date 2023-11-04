e1976e2d9eea9091609a8b6d78f88f175462a4a4
BhathiyaVandebona <malingabhathiya@gmail.com>
2023-11-04 18:05:17 +0530

    modified and added set time out to the the produce and consume functions to execute periodically

--- microservices_dev/messaging_server/index.js
+++ microservices_dev/messaging_server/index.js
@@ -1,5 +1,26 @@
-const Producer = require('../service_message_queue/producer');
+const Producer = require("../service_message_queue/producer"); // Import the Producer class
 
+// Create an instance of the Producer class
 const producer = new Producer();
-producer.setup();
-producer.produce('alerts-exchange', 'direct', 'notices', { message: "Hello, world" });
+
+// Set up the producer
+async function setupProducer() {
+  try {
+    await producer.setup();
+    console.log("Producer is connected and channel is created.");
+  } catch (error) {
+    console.error("Error setting up producer:", error);
+  }
+}
+
+setupProducer();
+
+// Example message to send
+const message = {
+  text: "Hi Ho, Silver!",
+};
+
+// Send the message to an exchange with a routing key
+setInterval(() => {
+  producer.produce("my_exchange", "direct", "my_routing_key", message);
+}, 1000);
--- microservices_dev/notification_service/index.js
+++ microservices_dev/notification_service/index.js
@@ -1,7 +1,27 @@
-const Consumer = require('../service_message_queue/consumer');
+const Consumer = require("../service_message_queue/consumer"); // Import the Consumer class
 
+// Create an instance of the Consumer class
 const consumer = new Consumer();
-consumer.setup('alerts-exchange', 'direct', 'notification-queue', 'notices');
-consumer.consume((data) => {
-    console.log(data);
-});
+
+// Set up the consumer
+async function setupConsumer() {
+  try {
+    // Set up the consumer to listen to the same exchange and routing key
+    await consumer.setup("my_exchange", "direct", "my_queue", "my_routing_key");
+    console.log(
+      "Consumer is connected, channel is created, and queue is bound."
+    );
+  } catch (error) {
+    console.error("Error setting up consumer:", error);
+  }
+}
+
+setupConsumer();
+
+// Define a callback function to handle incoming messages
+function handleMessage(message) {
+  console.log("Received message:", message);
+}
+
+// Start consuming messages
+setInterval(() => consumer.consume(handleMessage), 1000);
