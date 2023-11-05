42f834f2c00aaaa6152b0cf4da45be132c172a89
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2023-11-05 13:29:53 +0530

    change password functionality and authentication

--- microservices_dev/authentication_service/index.js
+++ microservices_dev/authentication_service/index.js
@@ -1,149 +1,192 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const fs = require('fs');
-const {v4: uuid } = require('uuid');
-const cors = require('cors'); 
-const data = require('./schema/data.json');
-const exisitingUsers = require('./schema/existing_users.json');
+require("dotenv").config();
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const cors = require("cors");
+const data = require("./schema/data.json");
+const exisitingUsers = require("./schema/existing_users.json");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-app.post('/login', (req, res) => {
- 
+app.post("/login", (req, res) => {
   const { username, pass } = req.body;
 
   for (var user of data) {
     if (user.username === username && user.password === pass) {
       res.statusCode = 200;
-      const userId= user.id;
+      const userId = user.id;
       const userrole = user.role;
-      res.json({message: "Login successful", userId, userrole});
+      const payload = {
+        phone: user.phone,
+        userrole: userrole,
+      };
+      const secretKey = process.env.JWT_TOKEN_SECRET; // Replace with a strong secret key for signing the token
+      const accessToken = jwt.sign(payload, secretKey, { expiresIn: "15m" }); // Access token expires in 1 hour
+      const refreshToken = jwt.sign(payload, secretKey, { expiresIn: "7d" }); // Refresh token expires in 1 day
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].id === userId) {
+          data[i].refreshToken = refreshToken;
+          break;
+        }
+      }
+      // Write the updated data back to the JSON file
+      fs.writeFileSync("./schema/data.json", JSON.stringify(data, null, 2));
+      res.json({ accessToken, refreshToken, userrole });
       return;
     }
   }
   res.statusCode = 401;
-  res.json({message: "Login failed"});
+  res.json({ message: "Login failed" });
 });
+
+app.get("/checkRefreshToken/:refreshToken", (req, res) => {
+//return user id with the token
+  const refreshToken = req.params.refreshToken;
+  for (var user of data) {
+    if (user.refreshToken === refreshToken) {
+      res.statusCode = 200;
+      res.json({ message: "Valid refresh token", phone: user.phone });
+      return;
+    }
+  }
+  res.statusCode = 403;
+  res.json({ message: "Invalid refresh token", userId: null });
+});
+
 //function to check whether username already exists
-app.get('/checkusername/:username', (req, res) => {
+app.get("/checkusername/:username", (req, res) => {
   const username = req.params.username;
   for (var user of data) {
     if (user.username == username) {
       res.statusCode = 200;
-     //return user id
-      res.json({message: "Username already exists"});
+      //return user id
+      res.json({ message: "Username already exists" });
       return;
     }
   }
   res.statusCode = 200;
-  res.json({message: "Username not found"});
+  res.json({ message: "Username not found" });
 });
 
-app.get('/checkemail/:email', (req, res) => {
+app.get("/checkemail/:email", (req, res) => {
   const email = req.params.email;
   for (var user of data) {
     if (user.email == email) {
       res.statusCode = 200;
-      res.json({message: "User with this email already exists"});
+      res.json({ message: "User with this email already exists" });
       return;
     }
   }
   res.statusCode = 200;
-  res.json({message: "Email not found"});
+  res.json({ message: "Email not found" });
 });
- 
-app.get('/checkphone/:phone', (req, res) => {
+
+app.get("/checkphone/:phone", (req, res) => {
   const phone = req.params.phone;
   for (var user of data) {
     if (user.phone == phone) {
       res.statusCode = 200;
-      res.json({message: "User with this phone number already exists"});
+      res.json({ message: "User with this phone number already exists" });
       return;
     }
   }
   res.statusCode = 200;
-  res.json({message: "Phone number not found"});
+  res.json({ message: "Phone number not found" });
 });
 
-app.get('/checkphoneValidity/:phone', (req, res) => {
+app.get("/checkphoneValidity/:phone", (req, res) => {
   //check if mobile number is in the existing users list
   const phone = req.params.phone;
   for (var user of exisitingUsers) {
     if (user.phone == phone) {
       res.statusCode = 200;
-      res.json({message: "Valid phone number"});
+      res.json({ message: "Valid phone number" });
       return;
     }
   }
   res.statusCode = 200;
-  res.json({message: "Not a valid SriTel phone number"});
+  res.json({ message: "Not a valid SriTel phone number" });
 });
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const fname = req.body.fname;
   const lname = req.body.lname;
   const username = req.body.username;
   const email = req.body.email;
-  const contact = req.body.contact;
+  const phone = req.body.contact;
   const password = req.body.pass;
   const address = req.body.address;
-  const role = 'user';
+  const role = "user";
+  const refreshToken = null;
   const id = uuid();
-  const user = { id, fname, lname, username, email, contact, password, address, role };
+  const user = {
+    id,
+    fname,
+    lname,
+    username,
+    email,
+    phone,
+    password,
+    address,
+    role,
+    refreshToken
+  };
   data.push(user);
-  fs.writeFileSync('./schema/data.json', JSON.stringify(data, null, 2));
+  fs.writeFileSync("./schema/data.json", JSON.stringify(data, null, 2));
   res.statusCode = 200;
-  res.json({message: "User added successfully"});
-  
+  res.json({ message: "User added successfully" });
 });
 
-app.get('/users', (req, res) => {
-  console.log('sending all the users');
+app.get("/users", (req, res) => {
+  console.log("sending all the users");
   res.statusCode = 200;
   res.json({ users: data });
 });
 
-app.get('/user/:id', (req, res) => {
-  console.log('sending a single user');
-  const userId = req.params.id;
+app.get("/user/:id", (req, res) => {
+  console.log("sending a single user");
+  const phone = req.params.id;
   for (user of data) {
-    if (user.id == userId) {
+    if (user.phone == phone) {
       res.statusCode = 200;
-      res.json({user: user});
+      res.json({ user: user });
       return;
     }
   }
   res.statusCode = 404;
-  res.json({message: "User cannot be found"});
+  res.json({ message: "User cannot be found" });
 });
 
 // create user
-app.post('/user', (req, res) => {
-  console.log('adding a new user');
+app.post("/user", (req, res) => {
+  console.log("adding a new user");
   let userData = req.body;
   try {
     userData.id = uuid();
     data.push(user);
-    fs.writeFileSync('./schema/data.json', JSON.stringify(data, null, 2));
-    console.log('Data written to file');
+    fs.writeFileSync("./schema/data.json", JSON.stringify(data, null, 2));
+    console.log("Data written to file");
     res.statusCode = 201;
-    res.json({user: userData});
+    res.json({ user: userData });
   } catch (err) {
-    console.error('Error writing file', err);
-    res.json({message: "Internal server error occurred"});
+    console.error("Error writing file", err);
+    res.json({ message: "Internal server error occurred" });
   }
 });
 
-app.put('/user/:id', (req, res) => {
-  res.send('Hello World!');
+app.put("/user/:id", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.patch('/user/:id', (req, res) => {
+app.patch("/user/:id", (req, res) => {
   console.log(req.hostname);
-  res.send('Hello World!');
+  res.send("Hello World!");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Auth service started : Listening on port http://localhost:${process.env.PORT}`);
+  console.log(
+    `Auth service started : Listening on port http://localhost:${process.env.PORT}`
+  );
 });
--- microservices_dev/router/index.js
+++ microservices_dev/router/index.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const axios = require('axios');
-require('dotenv').config();
-const cors = require('cors'); 
-
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 // Enable CORS for all routes or specific routes
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: 'GET,POST', // Add any other HTTP methods you need
-  credentials: true,   // Allow cookies and authentication headers
+  origin: "http://localhost:3000",
+  methods: "GET,POST", // Add any other HTTP methods you need
+  credentials: true, // Allow cookies and authentication headers
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
+    let response = await axios.get(
+      `http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`
+    );
     let data = await response.data;
     if (data) {
-      let intermediaResponse = await axios.get(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/users`);
+      let intermediaResponse = await axios.get(
+        `http://${data.serviceInfo.host}:${data.serviceInfo.port}/users`
+      );
       let intermediaData = await intermediaResponse.data;
       res.json(intermediaData.users);
     } else {
@@ -33,25 +39,57 @@ app.get('/users', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.get("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    let response = await axios.get(
+      `http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`
+    );
+    let data = await response.data;
+    if (data) {
+      let intermediaResponse = await axios.get(
+        `http://${data.serviceInfo.host}:${data.serviceInfo.port}/user/${userId}`
+      );
+      let intermediaData = await intermediaResponse.data;
+      res.json(intermediaData.user);
+    } else {
+      res.statusCode = 500;
+      res.json({ message: "An internal server error occurred" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
   const { username, pass } = req.body;
   try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
+    let response = await axios.get(
+      `http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`
+    );
     let data = await response.data;
- 
+
     if (data) {
       try {
-        let intermediaResponse = await axios.post(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/login`, {
-          username,
-          pass
-        });
+        let intermediaResponse = await axios.post(
+          `http://${data.serviceInfo.host}:${data.serviceInfo.port}/login`,
+          {
+            username,
+            pass,
+          }
+        );
         let intermediaData = await intermediaResponse.data;
+        res.cookie("jwt", intermediaData.refreshToken, {
+          httpOnly: true,
+          sameSite: "none",
+          secure: true,
+          maxAge: 24 * 60 * 60 * 1000,
+        });
         res.json(intermediaData);
       } catch (error) {
         res.statusCode = 401;
         res.json(error.response.data);
       }
-      
     } else {
       res.statusCode = 500;
       res.json({ message: "An internal server error occurred" });
@@ -61,22 +99,26 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const user_data = req.body;
   try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
+    let response = await axios.get(
+      `http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`
+    );
     let data = await response.data;
- 
+
     if (data) {
       try {
-        let intermediaResponse = await axios.post(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/register`,user_data);
+        let intermediaResponse = await axios.post(
+          `http://${data.serviceInfo.host}:${data.serviceInfo.port}/register`,
+          user_data
+        );
         let intermediaData = await intermediaResponse.data;
         res.json(intermediaData);
       } catch (error) {
         res.statusCode = 401;
         res.json(error.response.data);
       }
-      
     } else {
       res.statusCode = 500;
       res.json({ message: "An internal server error occurred" });
@@ -86,115 +128,108 @@ app.post('/register', async (req, res) => {
   }
 });
 
-
-
-app.get('/checkusername/:username', async (req, res) => {
-
-  const username = req.params.username;
+app.get("/refresh", async (req, res) => {
+  const cookies = req.cookies;
   try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
-    let data = await response.data;
- 
-    if (data) {
-      try {
-        let intermediaResponse = await axios.get(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/checkusername/${username}`);
-        let intermediaData = await intermediaResponse.data;
-        res.json(intermediaData);
-      } catch (error) {
-        res.statusCode = 401;
-        res.json(error.response.data);
+    if (!cookies?.jwt) return res.sendStatus(401);
+    const refreshToken = cookies.jwt;
+    try {
+      let response = await axios.get(
+        `http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`
+      );
+      let data = await response.data;
+      if (data) {
+        try {
+          let intermediaResponse = await axios.get(
+            `http://${data.serviceInfo.host}:${data.serviceInfo.port}/checkRefreshToken/${refreshToken}`
+          );
+          let intermediaData = await intermediaResponse.data;
+          const userrole = "user";
+          const phone = intermediaData.phone;
+         
+          // User is authenticated. Generate a JWT token with user information.
+          const payload = {
+            phone: phone,
+            userrole: userrole,
+          };
+     
+          jwt.verify(
+            refreshToken,
+            process.env.JWT_TOKEN_SECRET,
+            (err,tokenData) =>{
+              if(err || phone != tokenData.phone) return res.sendStatus(403);
+              const accessToken = jwt.sign(
+                payload,
+                process.env.JWT_TOKEN_SECRET,
+                { expiresIn: '15m' }
+              )
+              res.json({ accessToken, userrole, phone })
+            }
+          )
+        } catch (error) {
+          res.statusCode = 403;
+          res.json(error);
+        }
+      } else {
+        res.statusCode = 500;
+        res.json({ message: "An internal server error occurred" });
       }
-      
-    } else {
-      res.statusCode = 500;
-      res.json({ message: "An internal server error occurred" });
+    } catch (error) {
+      console.log(error);
     }
   } catch (error) {
-    console.log(error);
+    console.error("Error refreshing the token:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.get('/checkemail/:email', async (req, res) => {
-
-  const email = req.params.email;
+// Common function to hanfle registation validations
+async function checkData(req, res, endpoint, params) {
+  const dataParam = params;
   try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
-    let data = await response.data;
- 
+    const response = await axios.get(
+      `http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`
+    );
+    const data = await response.data;
+
     if (data) {
       try {
-        let intermediaResponse = await axios.get(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/checkemail/${email}`);
-        let intermediaData = await intermediaResponse.data;
+        const intermediaResponse = await axios.get(
+          `http://${data.serviceInfo.host}:${data.serviceInfo.port}/${endpoint}/${dataParam}`
+        );
+        const intermediaData = await intermediaResponse.data;
         res.json(intermediaData);
       } catch (error) {
-        res.statusCode = 401;
-        res.json(error.response.data);
+        res.status(401).json(error.response.data);
       }
-      
     } else {
-      res.statusCode = 500;
-      res.json({ message: "An internal server error occurred" });
+      res.status(500).json({ message: "An internal server error occurred" });
     }
   } catch (error) {
     console.log(error);
   }
+}
+
+// Route handlers using the common validation function
+app.get("/checkusername/:username", async (req, res) => {
+  checkData(req, res, "checkusername", req.params.username);
 });
 
-
-app.get('/checkphone/:phone', async (req, res) => {
-
-  const phone = req.params.phone;
-  try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
-    let data = await response.data;
- 
-    if (data) {
-      try {
-        let intermediaResponse = await axios.get(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/checkphone/${phone}`);
-        let intermediaData = await intermediaResponse.data;
-        res.json(intermediaData);
-      } catch (error) {
-        res.statusCode = 401;
-        res.json(error.response.data);
-      }
-      
-    } else {
-      res.statusCode = 500;
-      res.json({ message: "An internal server error occurred" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
+app.get("/checkemail/:email", async (req, res) => {
+  checkData(req, res, "checkemail", req.params.email);
 });
 
-app.get('/checkphoneValidity/:phone', async (req, res) => {
-
-  const phone = req.params.phone;
-  try {
-    let response = await axios.get(`http://localhost:${process.env.SERVICE_REGISTRY_PORT}/services/authenticationService`);
-    let data = await response.data;
- 
-    if (data) {
-      try {
-        let intermediaResponse = await axios.get(`http://${data.serviceInfo.host}:${data.serviceInfo.port}/checkphoneValidity/${phone}`);
-        let intermediaData = await intermediaResponse.data;
-        res.json(intermediaData);
-      } catch (error) {
-        res.statusCode = 401;
-        res.json(error.response.data);
-      }
-      
-    } else {
-      res.statusCode = 500;
-      res.json({ message: "An internal server error occurred" });
-    }
-  } catch (error) {
-    console.log(error);
-  }
+app.get("/checkphone/:phone", async (req, res) => {
+  checkData(req, res, "checkphone", req.params.phone);
 });
 
-
+app.get("/checkphoneValidity/:phone", async (req, res) => {
+  checkData(req, res, "checkphoneValidity", req.params.phone);
+});
 
 app.listen(process.env.PORT, () => {
-  console.log('Router service started : ',`Listening on http://localhost:${process.env.PORT}`)
+  console.log(
+    "Router service started : ",
+    `Listening on http://localhost:${process.env.PORT}`
+  );
 });
